ffe69514b75f25b33e3076d276e51ee7839332fb
CheetahDoesStuff <bravestcheetah@gmail.com>
2025-10-04 15:15:16 +0200

    added set func to mdl

--- src/shards/mdl/mdl.py
+++ src/shards/mdl/mdl.py
@@ -1,9 +1,11 @@
+from typing import Any
+
 class mdl:
     def __init__(self, depth: int = 2, size: int = 10, default = None) -> None:
         self.depth = depth
         self.size = size
         self.val = default
-        self.md_list = self._rec_mklist(depth)
+        self.md_list: Any = self._rec_mklist(depth)
 
     def _rec_mklist(self, d):
         if d == 1:
@@ -12,7 +14,7 @@ class mdl:
 
 
     def read(self, coords: tuple = ()):
-        if len(coords) > self.depth: return None
+        if len(coords) > self.depth: raise ValueError(f"Shards: Coords must have {self.depth} elements (Following the lists depth)")
 
         curr = self.md_list
         for coord in coords:
@@ -21,3 +23,9 @@ class mdl:
         return curr
     
     def set(self, coords: tuple, val = "0"):
+        if len(coords) > self.depth: raise ValueError(f"Shards: Coords must have {self.depth} elements (Following the lists depth)")
+
+        curr = self.md_list
+        for coord in coords[:-1]:
+            curr = curr[coord]
+        curr[coords[-1]] = val
