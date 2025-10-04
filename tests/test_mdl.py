35e6ac97ff07a23ca792dd1fa46f66f7f2e592fc
CheetahDoesStuff <bravestcheetah@gmail.com>
2025-10-04 19:45:45 +0200

    moved mdl full test to below mdl init test

--- tests/test_mdl.py
+++ tests/test_mdl.py
@@ -5,6 +5,9 @@ mdl = mdl(depth=2, size=3, default="cool")
 def test_mdl_init():
     assert mdl.read(coords=(1, 1)) == "cool" # type: ignore
 
+def test_mdl_init_full():
+    assert mdl.full() == "[['cool', 'cool', 'cool'], ['cool', 'cooler', 'cool'], ['cool', 'cool', 'cool']]" # type: ignore
+
 def test_mdl_write():
     mdl.set(coords=(1, 1), val="cooler") # type: ignore
     
