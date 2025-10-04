5d19fc9654ded2dbc6a246b76df3d93ce1b80dc8
CheetahDoesStuff <bravestcheetah@gmail.com>
2025-10-04 19:37:10 +0200

    added mdl test coverage

---
+++ tests/test_mdl.py
@@ -0,0 +1,12 @@
+from shards import mdl
+
+mdl = mdl(depth=2, size=3, default="cool")
+
+def test_mdl_init():
+    assert mdl.read(coords=(1, 1)) == "cool" # type: ignore
+
+def test_mdl_write():
+    mdl.set(coords=(1, 1), val="cooler") # type: ignore
+    
+    assert mdl.read(coords=(1, 1)) == "cooler" # type: ignore
+    assert mdl.read(coords=(1, 2)) == "cool" # type: ignore
